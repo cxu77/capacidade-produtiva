cf35b38849d7f502ddefce1bf97c097a3a0c6b7b
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-21 11:25:22 -0300

    Add files via upload

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -198,10 +198,12 @@ for i in range(len(gap_horas)):
                         
         elif turnos[i] == turnos[index]:
             for j in range(len(cal_linhas[0])):
-                if cal_linhas[i][j] > 0 and cal_linhas[index][j] > 0 and gap_horas[index] > 0:
-                    k = 24 - cal_linhas[i][j]
+                if cal_linhas[i][j] > 0 and cal_linhas[index][j] > 0 and cal_linhas[index][j] !=5 and gap_horas[index] > 0:    
+                    k = cal_linhas[index][j]
                     cal_linhas[i][j] += k
-                    cal_linhas[index][j] -= k 
+                    cal_linhas[index][j] -= k
+                    if cal_linhas[i][j] > 24:
+                        cal_linhas[index][j] = 24
                     gap_horas[index] -= k
                     gap_horas[i] += k
                     if gap_horas[i] >= 0:
