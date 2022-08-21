c9f5024d87a9822d36c9ff9aa1577289b564b2cf
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-21 19:41:52 -0300

    Add files via upload

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -193,17 +193,16 @@ if "LB04" in selecao and "LB09" in selecao:
     max_index = max(index_lb04, index_lb09)
     min_index = min(index_lb04, index_lb09)
     for j in range(len(cal_linhas[0])):
-        if gap_horas[min_index] >= 0:
-            if cal_linhas[min_index][len(cal_linhas[0]) -1 - j] > 0:
+        if cal_linhas[min_index][len(cal_linhas[0]) - 1 - j] > 0 and gap_horas[min_index] - cal_linhas[min_index][len(cal_linhas[0]) - 1 - j] >= 0:
                 gap_horas[min_index] -= cal_linhas[min_index][len(cal_linhas[0]) - 1 - j]
                 cal_linhas[min_index][len(cal_linhas[0]) -1 -  j] = 0
                 k = j
 
 
     for i in range(len(cal_linhas[0]) - k):
-        if cal_linhas[max_index][len(cal_linhas[0]) - 1 - k - i] > 0:
-            gap_horas[max_index] -= cal_linhas[max_index][len(cal_linhas[0]) - 1 - k - i]
-            cal_linhas[max_index][len(cal_linhas[0]) - 1- k - i] = 0
+        if cal_linhas[max_index][i] > 0:
+            gap_horas[max_index] -= cal_linhas[max_index][i]
+            cal_linhas[max_index][i] = 0
         
 for i in range(len(gap_horas)):
     if gap_horas[i] < 0:
