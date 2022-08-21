2790b39f77d15c790b312ea53d1b32152f2188fd
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-21 11:45:11 -0300

    Add files via upload

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -188,10 +188,10 @@ for i in range(len(gap_horas)):
         if turnos[i] < turnos[index]:
             for j in range(len(cal_linhas[0])):
                 if cal_linhas[i][j] > 0 and cal_linhas[index][j] > 0 and gap_horas[index] > 0:
-                    cal_linhas[i][j] += 6
-                    cal_linhas[index][j] -= 6 
-                    gap_horas[index] -= 6
-                    gap_horas[i] += 6
+                    cal_linhas[i][j] += 5
+                    cal_linhas[index][j] -= 5 
+                    gap_horas[index] -= 5
+                    gap_horas[i] += 5
                     if gap_horas[i] >= 0:
                         break
                         
@@ -199,14 +199,15 @@ for i in range(len(gap_horas)):
         elif turnos[i] == turnos[index]:
             for j in range(len(cal_linhas[0])):
                 if cal_linhas[i][j] > 0 and cal_linhas[index][j] > 0 and cal_linhas[index][j] !=5 and gap_horas[index] > 0:    
-                    k = cal_linhas[index][j]
-                    p = cal_linhas[i][j]
+                    k = (3-turnos[i])*5
+                    if k == 0: 
+                        k = 1
                     cal_linhas[i][j] += k
                     cal_linhas[index][j] -= k
-                    if cal_linhas[i][j] > 24:
-                        cal_linhas[i][j] = 24
+                    if cal_linhas[index][j] < 0:
+                        cal_linhas[index][j] = 0
                     gap_horas[index] -= k
-                    gap_horas[i] += cal_linhas[i][j] - p
+                    gap_horas[i] += k
                     if gap_horas[i] >= 0:
                         break
                         
@@ -215,9 +216,9 @@ for i in range(len(gap_horas)):
                 if cal_linhas[i][j] > 0 and cal_linhas[index][j] > 0 and gap_horas[index] > 0:
                     k =  24 - cal_linhas[i][j]
                     cal_linhas[i][j] += k
-                    cal_linhas[index][j] -= 6
+                    cal_linhas[index][j] -= 5
                     gap_horas[i] += k
-                    gap_horas[index] -= 6   
+                    gap_horas[index] -= 5   
                     if gap_horas[i] >= 0:
                         break
                     
