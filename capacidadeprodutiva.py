fad8e54da728a81aa1f43c732536a494d38a04e7
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-22 06:42:14 -0300

    Update capacidadeprodutiva.py

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -259,13 +259,10 @@ for i in range(0, no_linhas):
 for i in range(no_linhas):
     for j in range(len(cal_linhas[0])):
         if cal_linhas[i][j] == -1:
-            cal_linhas[i][j] = str(cal_linhas[i][j])
             cal_linhas[i][j] = "FERIADO"
         elif cal_linhas[i][j]== -2:
-            cal_linhas[i][j] = str(cal_linhas[i][j])
             cal_linhas[i][j] = "PREV"
         elif cal_linhas[i][j] == -3:
-            cal_linhas[i][j] = str(cal_linhas[i][j])
             cal_linhas[i][j] = "INV"
         elif cal_linhas[i][j] == -4:
             cal_linhas[i][j] = 0
