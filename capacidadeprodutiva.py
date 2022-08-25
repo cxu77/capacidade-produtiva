a63d4705cdbda872acf70e21a774c99e0264f954
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-25 08:51:54 -0300

    Update capacidadeprodutiva.py

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -297,7 +297,7 @@ gb.configure_columns(column_names=nome_colunas, editable = True, groupable = Tru
 #gb.configure_auto_height()
 go = gb.build()  
 results = AgGrid(data = cal, reload_data = False, gridOptions = go, enable_enterprise_modules=True, update_mode = GridUpdateMode.VALUE_CHANGED, data_return_mode = DataReturnMode.AS_INPUT )
-agregado = pd.DataFrame.from_dict(results)
+agregado = pd.DataFrame.from_dict(results["data"])
 #agregado['Horas'] = agregado.sum(axis=1)
 #agregado['Dias'] = agregado.sum(axis=1)/24
 #agregado.columns = ['Horas', 'Dias']
