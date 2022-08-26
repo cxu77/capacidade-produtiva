054641397291e33ff304dd71e7db8a36e9671e83
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-26 07:38:20 -0300

    Update capacidadeprodutiva.py

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -304,6 +304,7 @@ agregado.replace("FERIADO", 0., inplace = True)
 agregado.replace("PREV", 0., inplace = True)
 agregado.replace("INV", 0., inplace = True)
 agregado = agregado.groupby('Linhas').sum()
+agregado['Horas'] = agregado.sum(axis=-1)
 #agregado['Necessario'] = necess
 #agregado['HDO'] = hdo
 #agregado['Gap_Calculado'] = gap_horas
