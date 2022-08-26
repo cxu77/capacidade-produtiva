f317456b7b4645825c15c1a5a28d88cd7c6d1e5a
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-26 07:49:23 -0300

    Update capacidadeprodutiva.py

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -300,6 +300,7 @@ gb.configure_columns(column_names=nome_colunas, editable = True, groupable = Tru
 go = gb.build()  
 results = AgGrid(data = cal, reload_data = False, gridOptions = go, enable_enterprise_modules=True, update_mode = GridUpdateMode.VALUE_CHANGED, data_return_mode = DataReturnMode.AS_INPUT)
 agregado = pd.DataFrame.from_dict(results["data"])
+st.dataframe(agregado)
 agregado.replace("FERIADO", 0., inplace = True)
 agregado.replace("PREV", 0., inplace = True)
 agregado.replace("INV", 0., inplace = True)
