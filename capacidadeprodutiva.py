a5d5f0423b96fb6240baa978ea53bcc8a18fc534
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-29 08:25:12 -0300

    Update capacidadeprodutiva.py

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -322,7 +322,7 @@ agregado['Gap Horas'] = agregado['Gap Horas'].round(2)
 agregado['Gap Dias'] = agregado['Gap Dias'].round(0)
 #st.write(agregado)
 result_agregado = agregado[['Gap Horas', 'Gap Dias']]
-result_agregado.insert(0, 'Linhas', coluna1)
+result_agregado['Linhas'] = selecao
 st.write("Gap (em horas e em dias)")
 #gb = GridOptionsBuilder.from_dataframe(agregado["data"])
 #gb.configure_columns(columns_names =[], groupable=True, value=True, enableRowGroup=True, editable=False)
