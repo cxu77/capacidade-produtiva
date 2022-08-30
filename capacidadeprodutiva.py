41a7172dfa624b97f342ce7ea58cc2fcfc326411
cxu77 <111684941+cxu77@users.noreply.github.com>
2022-08-30 06:22:04 -0300

    Update capacidadeprodutiva.py

--- capacidadeprodutiva.py
+++ capacidadeprodutiva.py
@@ -99,8 +99,10 @@ def desc_4turnos(descontos, monthnumber):
     for i in range(len(descontos)):
         if calendar.weekday(date.today().year, monthnumber, i+1) % 7 < 5 and descontos[i] > 0 and descontos[i] !=5:
             descontos[i] -= 1
-        else:
+        elif calendar.weekday(date.today().year, monthnumber, i+1) % 7 >= 5 and descontos[i] > 0 and descontos[i] !=5:
             descontos[i] -= 2
+        else:
+            k = 0
     return descontos
 
 # leitura dos arquivos necessários
